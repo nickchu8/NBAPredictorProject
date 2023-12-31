e066da64da58e0c95121689434f696effc2504bd
Nick Chu <nyc8pv@gmail.com>
2023-12-31 13:08:58 -0800

    Completed playoff matchups dictionary

--- playoffmatchups.py
+++ playoffmatchups.py
@@ -2,187 +2,270 @@
 
 matchups = {
     '2000-2001':{
-        'East':[
-
+        'East':['sixers', 'pacers', 
+                'hornets', 'heat', 
+                'raptors', 'knicks',
+                  'bucks', 'magic'
         ],
-        'West':[
-
-        ]
+        'West':["spurs","timberwolves",
+    "lakers","trail blazers",
+    "kings","suns",
+    "mavericks","jazz"
+ ]
     },
     '2001-2002':{
-        'East':[
-
+        'East':['celtics','sixers',
+                'hornets','magic',
+                'pistons','raptors',
+                'nets','pacers'
         ],
-        'West':[
-
+        'West':['mavericks','timberwolves',
+                'lakers','blazers',
+                'kings','jazz',
+                'spurs','supersonics'
         ]
     },
     '2002-2003':{
-        'East':[
+        'East':['celtics', 'pacers',
+        'pistons', 'magic',
+        'nets', 'bucks',
+        'sixers', 'hornets'
 
         ],
-        'West':[
-
-        ]
+        'West':['mavericks', 'blazers',
+        'lakers', 'timberwolves',
+        'kings', 'jazz',
+        'spurs', 'suns']
     },
     '2003-2004':{
-        'East':[
-
-        ],
-        'West':[
-
-        ]
+        'East':['pistons', 'bucks',
+ 'pacers', 'celtics',
+ 'heat', 'hornets',
+ 'nets', 'knicks'],
+        'West':['lakers', 'rockets',
+ 'timberwolves', 'nuggets',
+ 'kings', 'mavericks',
+ 'spurs', 'grizzlies']
     },
     '2004-2005':{
-        'East':[
+        'East':['pistons', 'sixers',
+ 'pacers', 'celtics',
+ 'heat', 'nets',
+ 'wizards', 'bulls'],
+        'West':['mavericks', 'rockets',
+ 'suns', 'grizzlies',
+ 'spurs', 'nuggets',
+ 'supersonics', 'kings']
 
-        ],
-        'West':[
-
-        ]
     },
     '2005-2006':{
-        'East':[
-
-        ],
-        'West':[
-
-        ]
+        'East':['cavaliers', 'wizards',
+ 'pistons', 'bucks',
+ 'heat', 'bulls',
+ 'nets', 'pacers'],
+        'West':['mavericks', 'grizzlies',
+ 'clippers', 'nuggets',
+ 'suns', 'lakers',
+ 'spurs', 'kings']
     },
     '2006-2007':{
-        'East':[
-
-        ],
-        'West':[
-
-        ]
+        'East':['bulls', 'heat',
+ 'cavaliers', 'wizards',
+ 'pistons', 'magic',
+ 'nets', 'raptors']
+,
+        'West':['warriors', 'mavericks',
+ 'suns', 'lakers',
+ 'spurs', 'nuggets',
+ 'jazz', 'rockets']
     },
     '2007-2008':{
-        'East':[
+        'East':['celtics', 'hawks',
+ 'cavaliers', 'wizards',
+ 'pistons', 'sixers',
+ 'magic', 'raptors'],
+        'West':['lakers', 'nuggets',
+ 'hornets', 'mavericks',
+ 'spurs', 'suns',
+ 'jazz', 'rockets']
 
-        ],
-        'West':[
-
-        ]
     },
     '2008-2009':{
-        'East':[
+        'East':['hawks', 'heat',
+ 'celtics', 'bulls',
+ 'cavaliers', 'pistons',
+ 'magic', 'sixers']
+,
+        'West':['mavericks', 'spurs',
+ 'nuggets', 'hornets',
+ 'rockets', 'blazers',
+ 'lakers', 'jazz']
 
-        ],
-        'West':[
-
-        ]
     },
     '2009-2010':{
-        'East':[
+        'East':['hawks', 'bucks',
+ 'celtics', 'heat',
+ 'cavaliers', 'bulls',
+ 'magic', 'bobcats']
+,
+        'West':['lakers', 'thunder',
+ 'suns', 'blazers',
+ 'spurs', 'mavericks',
+ 'jazz', 'nuggets']
 
-        ],
-        'West':[
-
-        ]
     },
     '2010-2011':{
-        'East':[
+        'East':['hawks', 'magic',
+ 'celtics', 'knicks',
+ 'bulls', 'pacers',
+ 'heat', 'sixers']
+,
+        'West':['hawks', 'magic',
+ 'celtics', 'knicks',
+ 'bulls', 'pacers',
+ 'heat', 'sixers']
 
-        ],
-        'West':[
-
-        ]
     },
     '2011-2012':{
-        'East':[
+        'East':['celtics', 'hawks',
+ 'pacers', 'magic',
+ 'heat', 'knicks',
+ 'sixers', 'bulls']
+,
+        'West':['clippers', 'grizzlies',
+ 'lakers', 'nuggets',
+ 'thunder', 'mavericks',
+ 'spurs', 'jazz']
 
-        ],
-        'West':[
-
-        ]
     },
     '2012-2013':{
-        'East':[
+        'East':['bulls', 'nets',
+ 'pacers', 'hawks',
+ 'heat', 'bucks',
+ 'knicks', 'celtics']
+,
+        'West':['warriors', 'nuggets',
+ 'grizzlies', 'clippers',
+ 'thunder', 'rockets',
+ 'spurs', 'lakers']
 
-        ],
-        'West':[
-
-        ]
     },
     '2013-2014':{
-        'East':[
-
-        ],
-        'West':[
-
-        ]
+        'East':['nets', 'raptors',
+ 'pacers', 'hawks',
+ 'heat', 'bobcats',
+ 'wizards', 'bulls']
+,
+        'West':['clippers', 'warriors',
+ 'thunder', 'grizzlies',
+ 'blazers', 'rockets',
+ 'spurs', 'mavericks']
     },
     '2014-2015':{
-        'East':[
+        'East':['hawks', 'nets',
+ 'bulls', 'bucks',
+ 'cavaliers', 'celtics',
+ 'wizards', 'raptors']
+,
+        'West':['warriors', 'pelicans',
+ 'rockets', 'mavericks',
+ 'clippers', 'spurs',
+ 'grizzlies', 'blazers']
 
-        ],
-        'West':[
-
-        ]
     },
     '2015-2016':{
-        'East':[
+        'East':['hawks', 'celtics',
+ 'cavaliers', 'pistons',
+ 'heat', 'hornets',
+ 'raptors', 'pacers']
+,
+        'West':['warriors', 'rockets',
+ 'thunder', 'mavericks',
+ 'blazers', 'clippers',
+ 'spurs', 'grizzlies']
 
-        ],
-        'West':[
-
-        ]
     },
     '2016-2017':{
-        'East':[
+        'East':['celtics', 'bulls',
+ 'cavaliers', 'pacers',
+ 'raptors', 'bucks',
+ 'wizards', 'hawks']
+,
+        'West':['warriors', 'blazers',
+ 'rockets', 'thunder',
+ 'spurs', 'grizzlies',
+ 'jazz', 'clippers']
 
-        ],
-        'West':[
-
-        ]
     },
     '2017-2018':{
-        'East':[
+        'East':['celtics', 'bucks',
+ 'cavaliers', 'pacers',
+ 'sixers', 'heat',
+ 'raptors', 'wizards']
+,
+        'West':['warriors', 'spurs',
+ 'rockets', 'timberwolves',
+ 'pelicans', 'blazers',
+ 'jazz', 'thunder']
 
-        ],
-        'West':[
-
-        ]
     },
     '2018-2019':{
-        'East':[
+        'East':['celtics', 'pacers',
+ 'bucks', 'pistons',
+ 'sixers', 'nets',
+ 'raptors', 'magic']
+,
+        'West':['nuggets', 'spurs',
+ 'warriors', 'clippers',
+ 'rockets', 'jazz',
+ 'blazers', 'thunder']
 
-        ],
-        'West':[
-
-        ]
     },
     '2019-2020':{
-        'East':[
-
-        ],
-        'West':[
-
-        ]
+        'East':['celtics', 'sixers',
+ 'heat', 'pacers',
+ 'bucks', 'magic',
+ 'raptors', 'nets']
+,
+        'West':['nuggets', 'jazz',
+ 'rockets', 'thunder',
+ 'clippers', 'mavericks',
+ 'lakers', 'blazers']
     },
     '2020-2021':{
-        'East':[
+        'East':['hawks', 'knicks',
+ 'nets', 'celtics',
+ 'bucks', 'heat',
+ 'sixers', 'wizards']
+,
+        'West':['nuggets', 'blazers',
+ 'clippers', 'mavericks',
+ 'suns', 'lakers',
+ 'jazz', 'grizzlies']
 
-        ],
-        'West':[
-
-        ]
     },
     '2021-2022':{
-        'East':[
+        'East':['celtics', 'nets',
+ 'heat', 'hawks',
+ 'bucks', 'bulls',
+ 'sixers', 'raptors']
+,
+        'West':['mavericks', 'jazz',
+ 'warriors', 'nuggets',
+ 'grizzlies', 'timberwolves',
+ 'suns', 'pelicans']
 
-        ],
-        'West':[
-
-        ]
     },
     '2022-2023':{
-        'East':[
+        'East':['celtics', 'hawks',
+ 'heat', 'bucks',
+ 'knicks', 'cavaliers',
+ 'sixers', 'nets']
+,
+        'West':['nuggets', 'timberwolves',
+ 'warriors', 'kings',
+ 'lakers', 'grizzlies',
+ 'suns', 'clippers']
 
-        ],
-        'West':[
-
-        ]
     }
 }
